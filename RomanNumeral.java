13a1fabc819c1a2ea7067527f0cdc8d57c21605e
goergechneg <36049201+goergechneg@users.noreply.github.com>
2018-02-27 10:56:07 -0800

    Add files via upload

--- RomanNumeral.java
+++ RomanNumeral.java
@@ -7,118 +7,122 @@
 
 import static java.lang.System.*;
 
-
 public class RomanNumeral
 {
-	
-private Integer number;
+	private Integer number;
 	private String roman;
 
+	private final static int[] NUMBERS= {1000,900,500,400,100,90,
+													50,40,10,9,5,4,1};
 
-	private final static int[] NUMBERS= {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-
-
-	private final static String[] LETTERS = "L","XL","X","IX","V","IV","I"};
-
+	private final static String[] LETTERS = {"M","CM","D","CD","C","XC",
+												  "L","XL","X","IX","V","IV","I"};
 
 	public RomanNumeral(String str)
-	
-{
-
-
-setRoman(str);
-
-	
-}
-
-	
-public RomanNumeral(Integer orig)
-
 	{
 
 
-setNumber(orig);
+		setRoman(str);
+
+	}
+
+	public RomanNumeral(Integer orig)
+	{
+
+		setNumber(orig);
+
+
+	}
+
+	public void setNumber(Integer num)
+	{		
+		number = num;
+		
 
 
 
 	}
 
-	
-public void setNumber(Integer num)
-
+	public void setRoman(String rom)
 	{
 
+		roman = rom;
 
-number = num;
 
+	}
 
-	
-}
-
-	
-public void setRoman(String rom)
-
+	public Integer getNumber()
 	{
+		int value = 0;
+		if(roman.length() == 1)
+		{
+			for(int i = 0; i < LETTERS.length; i++){
+				if (roman.equals(LETTERS[i])) {
+					value = NUMBERS[i];
+				}
+				
+			}
+		} 
+		
+		else {
+			
+			while (roman.length() > 1)
+			{
+				
+				for(int i = 0; i<LETTERS.length; i++)
+				{
+					if(roman.length() > 1) {
+						if (roman.substring(0, 2).equals(LETTERS[i])){
+							roman = roman.substring(2, roman.length());
+							
+							value = value + NUMBERS[i];
+							
+							i = -1;
 
 
-roman = rom;
+						}
+						else if (roman.substring(0, 1).equals(LETTERS[i]))
+						{
+							roman = roman.substring(1,roman.length());
+							
+							value = value + NUMBERS[i];
+							
+							i = -1;
 
+						}
+					}
+					
+						}
+					}
+				}
+			
+			
+		return value;
+	}
 	
-}
+	public String getRoman() {
+		String roman2 = "";
+		
+		
+		while (number != 0) { 
+			for (int i = 0; i < NUMBERS.length; i++)
+			{
+				
+				if (number >= NUMBERS[i]) {
+					
+					number = number - NUMBERS[i];
+					roman2 = roman2 + LETTERS[i];
+					
+					i = -1;
 
-	
-public Integer getNumber()
-
-	{
-
-	  String newRoman = roman;
-
-	  int sum = 0;
-	    
-for(int i = 0; i < LETTERS.length; i++)
-	
-      {
-	      
- if(roman.startsWith(LETTERS[i]))
-	
-        {
-	  
-      sum += NUMBERS[i];
-
-	        newRoman = newRoman.replaceFirst(LETTERS[i],"");
-
-	        }
-
-	          
-	        }
-
-		return number;
-	
-}
+				}
+			}
+		}
+		return roman2;
+	}
 
 	public String toString()
-
 	{
-	  
-for(int i = 0; i < NUMBERS.length; i++)
-	 
-   {
-	     
- if(NUMBERS[i] < number)
-	 
-     {
-	      
-number -=   NUMBERS[i];
-	 
-     roman =  roman + LETTERS[i];
-
-	   
- }
-	
-}
-		
-return roman + "\n";
-	
-}
-
+		return getRoman() + "\n";
+	}
 }
